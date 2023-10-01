bb790cce2c955f92171655416ae2b9dc84e42e2a
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-10-02 01:47:55 +0300

    completed.

--- server/models.py
+++ server/models.py
@@ -1,28 +1,16 @@
-from sqlalchemy.ext.hybrid import hybrid_property
-from sqlalchemy_serializer import SerializerMixin
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
-from config import db, bcrypt
+db = SQLAlchemy()
+bcrypt = Bcrypt()
 
-class User(db.Model, SerializerMixin):
-    __tablename__ = 'users'
-
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
-    _password_hash = db.Column(db.String)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
 
-    @hybrid_property
-    def password_hash(self):
-        raise Exception('Password hashes may not be viewed.')
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
 
-    @password_hash.setter
-    def password_hash(self, password):
-        password_hash = bcrypt.generate_password_hash(
-            password.encode('utf-8'))
-        self._password_hash = password_hash.decode('utf-8')
-
-    def authenticate(self, password):
-        return bcrypt.check_password_hash(
-            self._password_hash, password.encode('utf-8'))
-
-    def __repr__(self):
-        return f'User {self.username}, ID: {self.id}'
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
