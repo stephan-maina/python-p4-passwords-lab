2053c85b2124ee5e662676cf4f5ac5ecf37861f4
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-10-02 01:48:38 +0300

    Update config.py

--- server/config.py
+++ server/config.py
@@ -1,19 +1,7 @@
-from flask import Flask
-from flask_bcrypt import Bcrypt
-from flask_migrate import Migrate
-from flask_restful import Api
-from flask_sqlalchemy import SQLAlchemy
+import os
 
-app = Flask(__name__)
-app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
-
-db = SQLAlchemy()
-migrate = Migrate(app, db)
-db.init_app(app)
-
-bcrypt = Bcrypt(app)
-
-api = Api(app)
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///your_database.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    BCRYPT_LOG_ROUNDS = 12
